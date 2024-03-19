db6a232e5b166d8c3a4b25ae2ec4f5ff5fce3092
TylerAddams <hoangliverstar@gmail.com>
2024-03-19 23:50:16 +0700

    first commit

---
+++ move.js
@@ -0,0 +1,25 @@
+function login()
+{
+    window.location.href="login.html";
+}
+function blog()
+{
+    window.location.href="blog.html";
+}
+
+function register()
+{
+    window.location.href="register.html";
+}
+function playmode()
+{
+window.location.href="playmode.html";
+}
+function shop()
+{
+    window.location.href="shop.html";
+}
+function faqs()
+{
+    window.location.href="faqs.html";
+}
